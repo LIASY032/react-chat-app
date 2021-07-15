7c7cc01131248fda37014be8097778cec663dce3
LIASY032 <liasy032@mymail.unisa.edu.au>
2021-07-15 17:12:36 +0930

    OpenConversation added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-
-import React, {useState} from 'react';
-import Dashboard from './components/Dashboard';
-
-import Login from './components/Login';
-import { ContactsProvider } from './contexts/ContactsProvider';
-import useLocalStorage from './hooks/useLocalStorage';
+import React, { useState } from "react";
+import Dashboard from "./components/Dashboard";
+import { ConversationsProvider } from "./contexts/ConversationsProvider";
+import Login from "./components/Login";
+import { ContactsProvider } from "./contexts/ContactsProvider";
+import useLocalStorage from "./hooks/useLocalStorage";
 
 function App() {
-  const [id, setId] = useLocalStorage("id")
+  const [id, setId] = useLocalStorage("id");
   const dashboard = (
     <ContactsProvider>
-<Dashboard id={id}/>
+      <ConversationsProvider id={id}>
+        <Dashboard id={id} />
+      </ConversationsProvider>
     </ContactsProvider>
-  )
-  return (
-    <div >
-      {id ?  dashboard : <Login onIdSubmit={setId}></Login>}
-     
-    </div>
   );
+  return <div>{id ? dashboard : <Login onIdSubmit={setId}></Login>}</div>;
 }
 
 export default App;
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import Sidebar from './Sidebar'
+import React from "react";
+import Sidebar from "./Sidebar";
+import OpenConversation from "./OpenConversation";
+import { useConversations } from "../contexts/ConversationsProvider";
 
-function Dashboard({id}) {
-    return (
-        <div className="d-flex" style={{height: "100vh"}}>
-            <Sidebar id={id}/>
-        </div>
-    )
+export default function Dashboard({ id }) {
+  const { selectedConversation } = useConversations();
+
+  return (
+    <div className="d-flex" style={{ height: "100vh" }}>
+      <Sidebar id={id} />
+      {selectedConversation && <OpenConversation />}
+    </div>
+  );
 }
-
-export default Dashboard
